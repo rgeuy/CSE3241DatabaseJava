97a5ca55895eed3eef08cc8d1dbfe8e3871c9da8
Logan Sauer <logan.sauer.012@gmail.com>
2021-02-28 18:18:10 -0500

    Implemented artist constructor and print info method

--- CSE3241DatabaseJava/src/Artist.java
+++ CSE3241DatabaseJava/src/Artist.java
@@ -1,4 +1,16 @@
-
 public class Artist {
 
+	private String fName, lName, stageName, bDate, gender;
+	
+	public Artist(fName, lName, stageName, bDate, gender) {
+		this.fName = fName;
+		this.lName = lName;
+		this.stageName = stageName;
+		this.bDate = bDate;
+		this.gender = gender;
+	}
+	
+	public void printArtistInfo() {
+		System.out.println("Artist information:\nName: " + fName + " " + lName + "\nStage Name: " + stageName + "\nBirthday: " + bDate + "\nGender: " + gender);
+	}
 }
