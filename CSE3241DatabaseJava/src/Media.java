e56c88b8b053ae0ffd7d1284ef5fc6fb053aa7bd
Logan Sauer <logan.sauer.012@gmail.com>
2021-02-28 18:56:04 -0500

    Added media class

---
+++ CSE3241DatabaseJava/src/Media.java
@@ -0,0 +1,16 @@
+public class Media {
+	private String medType, delvDate;
+	private int copies;
+	private float price;
+	
+	public Media(String media, int copies, float price, String del) {
+		medType = media;
+		this.copies = copies;
+		this.price = price;
+		delvDate = del;
+	}
+	
+	public void printMediaInfo() {
+		System.out.println("Ordered Media Information:\n\nMedia Type: " + medType + "\nNumber of copies: " + copies + "\nPrice: " + price + "\nExpected Delivery Date: " + delvDate);
+	}
+}
